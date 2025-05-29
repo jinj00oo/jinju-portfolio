ce5ebe4935c89def94ef786f874e9427c82706dc
jinj00oo <icems0428@naver.com>
2025-05-29 16:20:05 +0900

    style: 반응형 여백 및 정렬 수정style: 반응형 여백 및 정렬 수정

--- src/components/SkillItem.tsx
+++ src/components/SkillItem.tsx
@@ -8,7 +8,7 @@ const SkillItem = ({ skill }: SkillItemProps) => {
   return (
     <div className="w-100 h-40 p-5 rounded-4xl bg-[#FFFDF6] shadow-md hover:shadow-lg">
       <div className="flex w-full h-full items-center justify-center gap-5">
-        <div className="flex flex-col w-50 items-center justify-center border-r-2 border-gray-500 pr-4">
+        <div className="flex flex-col w-28 shrink-0 items-center justify-center border-r-2 border-gray-500 pr-4">
           <div className="rounded-full bg-[#FFFDF6] flex items-center justify-center">
             <img className="w-12 h-12" src={skill.image} alt={skill.name} />
           </div>
